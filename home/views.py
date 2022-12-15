c78a5238ef4e3ebf856a498c2e3053dd61c5fb4d
furkanyapan <furkanyapan@gmail.com>
2022-12-15 23:26:39 +0300

    contact us form added

--- home/views.py
+++ home/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
-from home.models import Setting
+from home.models import Setting, ContactFormu, ContactFormMessage
 
 
 # Create your views here.
@@ -21,6 +22,21 @@ def referanslar(request):
     return render(request, 'referanslarimiz.html', context)
 
 def iletisim(request):
+
+    if request.method == 'POST':
+        form = ContactFormu(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage()
+            data.name = form.cleaned_data['name']
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            messages.success(request, "Mesajınız Gönderilmiştir")
+            return HttpResponseRedirect ('/iletisim')
+
     setting = Setting.objects.get(pk=1)
-    context = {'setting': setting}
+    form = ContactFormu()
+    context = {'setting': setting, 'form': form}
     return render(request, 'iletisim.html', context)
