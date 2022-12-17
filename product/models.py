ca8c1061e0b63c7acf840ee7b4f0bbdd3e8e00f1
furkanyapan <furkanyapan@gmail.com>
2022-12-17 18:36:11 +0300

    added admin panel category tree

--- product/models.py
+++ product/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.utils.safestring import mark_safe
+from mptt.fields import TreeForeignKey
+from mptt.models import MPTTModel
 
 
 # Create your models here.
 
-class Category(models.Model):
+class Category(MPTTModel):
     STATUS = (
         ('True', 'Evet'),
         ('False', 'Hayir'),
@@ -15,11 +17,19 @@ class Category(models.Model):
     image = models.ImageField(blank=True, upload_to='images/')
     status = models.CharField(max_length=10, choices=STATUS)
     slug = models.SlugField()
-    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+    parent = TreeForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
+    class MPTTMeta:
+        order_insertion_by = ['title']
     def __str__(self):
-        return self.title
+        full_path = [self.title]
+        k = self.parent
+        while k is not None:
+            full_path.append(k.title)
+            k = k.parent
+        return ' >> '.join(full_path[::-1])
     def image_tag(self):
         return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
     image_tag.short_description = 'Image'
